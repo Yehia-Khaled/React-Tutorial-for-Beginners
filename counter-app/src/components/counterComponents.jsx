ad060a282aa91100f5bd66aad146d12edb5c1559
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-10-06 16:39:18 +0200

    11- Setting Attributes

--- counter-app/src/components/counterComponents.jsx
+++ counter-app/src/components/counterComponents.jsx
@@ -3,12 +3,22 @@ import React, { Component } from "react";
 class Counter extends React.Component {
   state = {
     count: 0,
+    // imageUrl: "https://picsum.photos/200",
   };
+
+  // styles = {
+  //   fontSize: 15,
+  //   fontWeight: "bold",
+  // };
+
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imageUrl} alt="" /> */}
+        <br></br>
+        {/* style={{ fontSize: 20 }}  */}
+        <span className="badge badge-primary m-3">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
