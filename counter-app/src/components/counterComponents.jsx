a6ae2ead0a39af8a5ab9c5495833563580409f1a
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-10-06 17:22:25 +0200

    14- Conditional Rendering

--- counter-app/src/components/counterComponents.jsx
+++ counter-app/src/components/counterComponents.jsx
@@ -5,7 +5,7 @@ class Counter extends React.Component {
   state = {
     count: 0,
     // imageUrl: "https://picsum.photos/200",
-    tags: ["tag1", "tag2", "tag3"],
+    tags: [],
   };
 
   // styles = {
@@ -13,25 +13,27 @@ class Counter extends React.Component {
   //   fontWeight: "bold",
   // };
 
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags! </p>;
+
+    return this.state.tags.map((tag) => <li key={tag}> tag</li>);
+  }
+
   render() {
     return (
       <React.Fragment>
-        {/* <img src={this.state.imageUrl} alt="" /> */}
-        <br></br>
-        {/* style={{ fontSize: 20 }}  */}
+        {/*         <img src={this.state.imageUrl} alt="" />
+         style={{ fontSize: 20 }}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-        {/* 13- Rendering Lists */}
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}> tag</li>
-          ))}
-        </ul>
+          */}
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        <ul>{this.renderTags()}</ul>
       </React.Fragment>
     );
   }
 
-  //12- Rendering Classes Dynamically
+  /*   //12- Rendering Classes Dynamically
   getBadgeClasses() {
     let classes = "badge  m-3 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
@@ -41,7 +43,7 @@ class Counter extends React.Component {
   formatCount() {
     const { count } = this.state;
     return count === 0 ? "Zero" : count;
-  }
+  } */
 }
 
 export default Counter;
