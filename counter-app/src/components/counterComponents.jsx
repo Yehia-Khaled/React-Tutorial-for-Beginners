28c3a1e7ebd947cf834ac57f114a70b1c61214a9
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-10-06 17:02:31 +0200

    13- Rendering Lists

--- counter-app/src/components/counterComponents.jsx
+++ counter-app/src/components/counterComponents.jsx
@@ -1,9 +1,11 @@
+// eslint-disable-next-line no-unused-vars
 import React, { Component } from "react";
 
 class Counter extends React.Component {
   state = {
     count: 0,
     // imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"],
   };
 
   // styles = {
@@ -19,6 +21,12 @@ class Counter extends React.Component {
         {/* style={{ fontSize: 20 }}  */}
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        {/* 13- Rendering Lists */}
+        <ul>
+          {this.state.tags.map((tag) => (
+            <li key={tag}> tag</li>
+          ))}
+        </ul>
       </React.Fragment>
     );
   }
