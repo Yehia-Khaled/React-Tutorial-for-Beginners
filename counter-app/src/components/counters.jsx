e37b6f7ff678bf2218e21505a96c127805e2df88
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-10-14 17:14:58 +0200

    20- Composing Components Codes and comments

---
+++ counter-app/src/components/counters.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Counter from "./counterComponents";
+
+class Counters extends React.Component {
+  state={
+    counters:[
+      {id:1,value:0},
+      {id:2,value:3},
+      {id:3,value:0},
+      {id:4,value:0}
+    ]
+  }
+  render() {
+    return (
+      <div>
+        {this.state.counters.map(counter=><Counter key={counter.id}/>)}
+      </div>
+    );
+  }
+}
+
+export default Counters;
