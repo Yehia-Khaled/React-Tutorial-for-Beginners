ed63a75617b7bfcc7964b83b2daa8975cab8b733
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-10-16 00:02:17 +0200

    21- Passing Data to Components Codes and comments

--- counter-app/src/components/counterComponents.jsx
+++ counter-app/src/components/counterComponents.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 
 class Counter extends React.Component {
   state = {
-    count: 0,
+    value: this.props.value,
     // imageUrl: "https://picsum.photos/200",
     // tags: ["tag1",'tag2','tag3','tag'],
   };
@@ -12,7 +12,7 @@ class Counter extends React.Component {
   //   fontSize: 15,
   //   fontWeight: "bold",
   // };
-/*  constructor(){
+  /*  constructor(){
     super(); //because we add constructor in this child class "class Counter extends React.Component " ,first we have to
     //call the constructor of the parent class using the super keyword
   //this is method is called when this object "class Counter extends React.Component " is created
@@ -22,14 +22,14 @@ class Counter extends React.Component {
 
 
 }*/
-/*
+  /*
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags! </p>;
 
     return this.state.tags.map((tag) => <li key={tag}> tag</li>);
   }
 */
-  handleIncrement= ( ) =>{
+  handleIncrement = () => {
     //16- Binding Event Handlers
     //arrow function don't rebind to this key word ,they inherited
     //using an arrow function is cleaner and simpler adding a custom constructor and rebinding every event handler manually
@@ -39,36 +39,38 @@ class Counter extends React.Component {
 
     //17- Updating the State
     // console.log(product)
-    this.setState({ count:this.state.count +1})
-  }
-
+    this.setState({ value: this.state.value + 1 });
+  };
 
   render() {
+    // console.log('props',this.props) test props
     return (
       <div>
-                 {/*<img src={this.state.imageUrl} alt="" />*/}
-         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        {/*<img src={this.state.imageUrl} alt="" />*/}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         {/*we didn't call this.handleIncrement() we paa a reference to it this.handleIncrement*/}
         <button
-            onClick={this.handleIncrement} //()=>this.handleIncrement({id:1})
-            className="btn btn-secondary btn-sm"
-        >Increment </button>
+          onClick={this.handleIncrement} //()=>this.handleIncrement({id:1})
+          className="btn btn-secondary btn-sm"
+        >
+          Increment{" "}
+        </button>
 
-       {/* {this.state.tags.length === 0 && "Please create a new tag!"}
+        {/* {this.state.tags.length === 0 && "Please create a new tag!"}
         <ul>{this.renderTags()}</ul>*/}
       </div>
     );
   }
 
-     // 12- Rendering Classes Dynamically
+  // 12- Rendering Classes Dynamically
   getBadgeClasses() {
     let classes = "badge  m-3 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
