714924fa8de2afb3b5f944a8a289615504d4888f
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-10-19 13:20:00 +0200

    36- Unmounting Phase and comments

--- counter-app/src/components/counterComponents.jsx
+++ counter-app/src/components/counterComponents.jsx
@@ -41,6 +41,17 @@ class Counter extends React.Component {
     // console.log(product)
     this.setState({ value: this.state.value + 1 });
   };*/
+    componentDidUpdate(prevProps, prevState,) {
+      console.log('PrevProps',prevProps)
+      console.log('PrevState',prevState)
+      if(prevProps.counter.value !==this.props.counter.value){
+        //Ajax call and get new data from the server
+      }
+    }
+
+    componentWillUnmount() {//this method ia called just before component is remove from the dom
+      console.log('Counter -Unmount')
+    }
 
   render() {
     // console.log('props',this.props) //test props
@@ -54,12 +65,12 @@ class Counter extends React.Component {
         {/*we didn't call this.handleIncrement() we paa a reference to it this.handleIncrement*/}
         <button
           onClick={()=>this.props.onIncrement(this.props.counter)} //()=>this.handleIncrement({id:1})
-          className="btn btn-secondary btn-sm"
+          className="btn btn-secondary btn-sm m-2"
         >
           Increment
         </button>
         <button
-            className="btn btn-danger btn-sm m-2"
+            className="btn btn-danger btn-sm m-3"
             onClick={()=>this.props.onDelete(this.props.counter.id)}
         >Delete</button>
 
